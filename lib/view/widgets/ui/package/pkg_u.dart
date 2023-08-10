6a7cbd36c733d229feac2348b010dce0733a78b4
Souta Sugimoto <s-sugimoto@softnext.co.jp>
2023-08-10 15:10:18 +0900

    不要ファイルの削除

--- lib/view/widgets/ui/package/pkg_u.dart
+++
@@ -1,3 +0,0 @@
-export 'package:flutter/material.dart';
-
-export 'package:flutter_riverpod/flutter_riverpod.dart';
--- lib/view_model/package/pkg_vm.dart
+++
@@ -1,5 +0,0 @@
-export 'dart:convert';
-
-export 'package:flutter_riverpod/flutter_riverpod.dart';
-
-export '../../model/pokemon_state.dart';
